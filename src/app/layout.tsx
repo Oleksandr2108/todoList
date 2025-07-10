e3e6762f1ee14aa3a61443d8a2d1617769f88c6a
Oleksandr Yepik <sepika534@gmail.com>
2025-07-11 01:11:53 +0300

    fix6

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,8 @@
 "use client";
 
-
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Provider } from "react-redux";
 import { store } from "@/store";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-
 
 export default function RootLayout({
   children,
@@ -26,11 +12,7 @@ export default function RootLayout({
   return (
     <Provider store={store}>
       <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          {children}
-        </body>
+        <body className={` antialiased`}>{children}</body>
       </html>
     </Provider>
   );
